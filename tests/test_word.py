6d7b1e77d976a14373882d5c9a7342e8bab4d0c1
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2021-01-21 14:13:59 -0400

    Add test for silent u letters

--- tests/test_word.py
+++ tests/test_word.py
@@ -28,3 +28,35 @@ def test_check_silent_h(text, expected):
     observed = word._check_silent_h()
 
     assert observed == expected
+
+@pytest.mark.parametrize(
+    ("text", "expected"),
+    [
+        # no us
+        ("oso", 0),
+        ("perro", 0),
+        # non silent us
+        ("urbano", 0),
+        ("contigua", 0),
+        ("uruguayo", 0),
+        # silent us
+        ("guiso", 1),
+        ("dengue", 1),
+        ("quieto", 1),
+        ("quebrar", 1),
+        ("panqueque", 2),
+        ("quisquilloso", 2),
+        # silent and non silent us
+        ("huelguista", 1),
+        ("conjugues", 1),
+        ("quietud", 1),
+        ("aunque", 1),
+    ]
+)
+def test_check_silent_u(text, expected):
+    """Test the check_silent_u function with different types of cases."""
+
+    word = Word(text)
+    observed = word._check_silent_u()
+
+    assert observed == expected
